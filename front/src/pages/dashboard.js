3f0445de27a758b0a38ce12e663ce2e0b0985f5a
Ragnar Valdre <roki1567@tlu.ee>
2022-11-08 01:45:16 +0200

    Fixes with dialog (handlers not done)

--- front/src/pages/dashboard.js
+++ front/src/pages/dashboard.js
@@ -1,43 +1,37 @@
-import { Grid, Paper, Button,Divider} from "@mui/material";
-import { Box, Container} from "@mui/system";
-import { styled } from '@mui/material/styles';
+import * as React from 'react';
+import { Button,Divider,Typography} from "@mui/material";
+import { Box } from "@mui/system";
+import Dialog, { DialogProps } from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+
 import AddIcon from '@mui/icons-material/Add';
+import Add from "@mui/icons-material/Add";
 
 export default function Dashboard() {
-
-
-    const Item = styled(Paper)(({ theme }) => ({
-        backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
-        ...theme.typography.body2,
-        padding: theme.spacing(1),
-        color: theme.palette.text.secondary,
-      }));
+    
+    
 
     return (
-        <Container component="div" maxWidth="xs" sx={{ minWidth: "20rem", marginLeft:0, marginTop:3}} >
-            <Paper elevation={3} sx={{ p: 1}}>
-                <Grid container spacing={24}>
-                    <Grid item xs={6.5} sx={{display:"flex", alignItems:"center", justifyContent:"left"}}>
-                        <Item sx={{display:"flex", boxShadow:"none", fontSize:16}}>To-do list</Item>
-                        <Item sx={{display:"none", boxShadow:"none", padding:0, fontSize:16, border:1, borderRadius:'50%', bgcolor:"#FA8072"}}></Item>           
-                    </Grid>
-                        
-                    <Grid item xs={4.5} sx={{display:"flex", alignItems:"center", justifyContent:"center"}}>
-                        <Item sx={{display:"flex", textAlign:"center", boxShadow:"none"}}>
-                            <Button variant="outlined" style={{maxWidth: '40px', maxHeight: '40px', minWidth: '40px', minHeight: '40px', borderColor: "black", color:"black"}}>Me</Button>
-                        </Item>
-                        <Item sx={{display:"flex", textAlign:"center", boxShadow:"none"}}>
-                            <Button variant="outlined" style={{maxWidth: '40px', maxHeight: '40px', minWidth: '40px', minHeight: '40px', borderColor: "black", color:"black"}}>
-                                <AddIcon />    
-                            </Button>
-                        </Item>
-                    </Grid>
-                </Grid>
+        <Box sx={{display:'flex',padding: 3}}>
+            <Box sx={{display: 'flex' ,border: 'solid', border: 1, minWidth: 425 ,justifyContent:'center'}}>
+                <Typography sx={{padding:1, display:'flex', alignItems:'center'}}>To-do list</Typography>
+                <Button variant="outlined" size="small" sx={{margin:1, marginLeft: 'auto', color:'black', borderColor:'black', minWidth: '36px', padding:'5px'}}>Me</Button>
+                <Button variant="outlined" size="small" sx={{margin:1, color:'black', borderColor:'black', minWidth: '36px', padding:'5px'}}>
+                    <AddIcon></AddIcon>
+                </Button>
+
                 <Divider></Divider>
 
-
-            </Paper>
-            
-        </Container>
+                <Dialog>
+                    <DialogContent>
+                        <DialogContentText>
+                            
+                        </DialogContentText>
+                    </DialogContent>
+                </Dialog>
+            </Box>
+        </Box>
     )
 }
