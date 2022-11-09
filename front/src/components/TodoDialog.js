0e0ab9a58377555da4162ced09bcd744f6906803
Ragnar Valdre <roki1567@tlu.ee>
2022-11-09 16:30:42 +0200

    To-do PopUp FE done
    BE connection not finished yet

--- front/src/components/TodoDialog.js
+++ front/src/components/TodoDialog.js
@@ -2,49 +2,173 @@ import React, { useState } from 'react';
 import { 
     Button,
     Dialog, 
-    DialogActions, 
+    DialogActions,
     DialogContent, 
     DialogTitle, 
-    FormHelperText, 
     TextField, 
-    Box } from '@mui/material';
-    import AddIcon from '@mui/icons-material/Add';
-    import axios from "axios"; 
+    Box,
+    Snackbar, 
+    Alert } from '@mui/material';
+import AddIcon from '@mui/icons-material/Add';
+import axios from "axios"; 
 
-    export default function BookmarkDialog({getData}){
-        const [open, setOpen] = useState(false)
+export default function BookmarkDialog({getData}){
+    const [open, setOpen] = useState(false)
+    const [snackOpen, setSnackOpen] = useState(false)
 
-        const form = {
-            title: "",
-            description: "",
-            date: "",
-            assigned: ""
-        }
-
-        const [formValue, setFormValue] = useState(form)
-
-        const handleFormChange = e => {
-            const { value, name } = e.target
-            const newValue = {
-                ...formValue,
-                [name]: value
-            }
-            setFormValue(newValue)
-        }
-
-        const handleClickOpen = () => {
-            setOpen(true)
-        }
-
-        const handleClose = () => {
-            setOpen(false)
-        }
-        
-        return(
-            
-            <>
-                <Button variant="outlined" size="small" sx={{margin:1, color:'black', borderColor:'black', minWidth: '36px', padding:'5px'}}><AddIcon></AddIcon></Button>
-            </>
-        )
-        
+    const form = {
+        title: "",
+        description: "",
+        date: "",
+        assigned: ""
     }
+
+    const snackbar = {
+        text: "",
+        severity: ""
+    }
+
+    const [formValue, setFormValue] = useState(form)
+    const [snackbarInfo, setSnackbarInfo] = useState(snackbar)
+
+    const handleFormChange = e => {
+        const { value, name } = e.target
+        const newValue = {
+            ...formValue,
+            [name]: value
+        }
+        setFormValue(newValue)
+    }
+
+    const handleClickOpen = () => {
+        setOpen(true)
+    }
+
+    const handleClose = () => {
+        setOpen(false)
+    }
+
+    const handleSnackClose = () => {
+        setSnackOpen(false)
+    }
+
+    const handleSubmit = e => {
+        e.preventDefault()
+        if(formValue.title && formValue.description && formValue.date && formValue.assigned){
+            axios.post("http://localhost:8080/todo/add", {
+                title: formValue.title,
+                description: formValue.description,
+                date: formValue.date,
+                assigned: formValue.assigned
+            })
+            .then(function(response){
+                console.log(response)
+                if(typeof response.data === "object" && response.data !== null){
+                    setSnackOpen(true)
+                    setSnackbarInfo({
+                        text: "Successfully saved!",
+                        severity: "success"
+                    })
+                    setOpen(false)
+                    getData()
+                } else if (typeof response.data === "string" && response.data !== null){
+                    setSnackOpen(true)
+                    setSnackbarInfo({
+                        text: response.data,
+                        severity: "info"
+                    })
+                } else {
+                    setSnackOpen(true)
+                    setSnackbarInfo({
+                        text: "Something went wrong!",
+                        severity: "error"
+                    })
+                }
+            })
+            .catch(function(err){
+                console.log(err)
+                setSnackOpen(true)
+                setSnackbarInfo({
+                    text: "Somethin went wrong!",
+                    severity: "error"
+                })
+                setOpen(false)
+            })
+        }
+    }
+    
+    return(
+        
+        <>
+            <Button variant="outlined" onClick={handleClickOpen} size="small" sx={{margin:1, color:'black', borderColor:'black', minWidth: '36px', padding:'5px'}}><AddIcon></AddIcon></Button>
+            <Dialog open={open} onClose={handleClose}>
+                <Box component="form" onSubmit={handleSubmit} noValidate>
+                    <DialogTitle>Add a new to-do assignment</DialogTitle>
+                    <DialogContent sx={{display: "flex", flexDirection: "column", gap: "0.5rem"}}>
+                        <TextField 
+                            value={formValue.title}
+                            onChange={e => handleFormChange(e)}
+                            required
+                            id="title"
+                            label="Title"
+                            name="title"
+                        />
+                        <TextField 
+                            value={formValue.description}
+                            onChange={e => handleFormChange(e)}
+                            required
+                            id="description"
+                            label="Assignment description"
+                            name="description"    
+                        />
+                       
+                        <TextField 
+                            InputLabelProps={{ shrink: true }}
+                            type="date"
+                            value={formValue.date}
+                            onChange={e => handleFormChange(e)}
+                            required
+                            id="date"
+                            label="Select due date"
+                            name="date"
+
+                        />
+                    
+                        <TextField
+                            value={formValue.assigned}
+                            onChange={e => handleFormChange(e)}
+                            required
+                            id="assigned"
+                            label="Assign a person"
+                            name="assigned"
+                        />
+                    </DialogContent>
+                    <DialogActions>
+                        <Button 
+                            onClick={handleClose}
+                            size="normal"
+                            variant="contained"
+                            margin="normal"
+                            >
+                            Cancel
+                        </Button>
+                        <Button 
+                            type="submit"
+                            size="normal"
+                            variant="contained"
+                            margin="normal"
+                            >
+                            Save
+                        </Button>
+                    </DialogActions>
+                    <Snackbar open={snackOpen} autoHideDuration={3000} onClose={handleSnackClose}>
+                        <Alert onClose={handleSnackClose} severity={snackbarInfo.severity}>
+                            {snackbarInfo.text}
+                        </Alert>
+                    </Snackbar>
+                </Box>
+            </Dialog>
+        </>
+    )
+    
+}
--- front/src/pages/dashboard.js
+++ front/src/pages/dashboard.js
@@ -1,8 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { Button,Divider,Paper,Typography} from "@mui/material";
 import { Box } from "@mui/system";
-import Dialog, { DialogProps } from '@mui/material/Dialog';
-import DialogActions from '@mui/material/DialogActions';
+import Dialog from '@mui/material/Dialog';
 import DialogContent from '@mui/material/DialogContent';
 import DialogContentText from '@mui/material/DialogContentText';
 import axios from 'axios';
@@ -31,7 +30,7 @@ export default function Dashboard() {
     return (
         <Box sx={{display:'flex',padding: 3}}>
             <Paper elevation={7} sx={{display: 'flex' , minWidth: 450 ,justifyContent:'center'}}>
-                <Typography sx={{padding:1, display:'flex', alignItems:'center'}}>To-do list <Typography sx={{display:'flex', border:'solid', border: 1, borderRadius:'50%', background: 'red', marginLeft:'5px', padding:'2px'}}>5</Typography></Typography>
+                <Typography sx={{padding:1, display:'flex', alignItems:'center'}}>To-do list <Typography sx={{display:'flex', border:'solid 1', borderRadius:'50%', background: 'red', marginLeft:'5px', padding:'2px'}}>5</Typography></Typography>
                 <Button variant="outlined" size="small" sx={{margin:1, marginLeft: 'auto', color:'black', borderColor:'black', minWidth: '36px', padding:'5px'}}>Me</Button>
                 <TodoDialog getData={getData}/>
 
