ff844c8b004f211fc58117762c4c3738556c8e71
klillenb <kertlillenberk@gmail.com>
2022-11-19 00:52:08 +0200

    initial calendar

--- front/src/pages/Schedule.js
+++ front/src/pages/Schedule.js
@@ -1,3 +1,21 @@
+import FullCalendar from '@fullcalendar/react'
+import dayGridPlugin from '@fullcalendar/daygrid'
+import { Box, Typography } from '@mui/material'
+
 export default function Schedule() {
-    return <h1>Schedule</h1>
+    return(
+        <>
+        <Box sx={{ padding: "2%" }}>
+            <Typography variant="h4">Legend icons here</Typography>
+        </Box>
+        <Box sx={{ padding: "0 2% 2% 2%" }}>
+            <FullCalendar
+                plugins={[ dayGridPlugin ]}
+                initialView="dayGridMonth"
+                firstDay={1}
+                contentHeight={500}
+            />  
+        </Box>
+        </>
+    )
 }
