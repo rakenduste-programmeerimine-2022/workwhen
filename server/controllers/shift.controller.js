1ec8d80b305c171e0f20020fc0f64ad2d8fc8032
klillenb <kertlillenberk@gmail.com>
2022-11-29 23:11:30 +0200

    Squashed commit of the following:
    commit 512b802331e4783f147dac8b1c7ce3a7a5986531
    Author: klillenb <kertlillenberk@gmail.com>
    Date:   Tue Nov 29 23:07:37 2022 +0200
    
        normalizing FE input
    
    commit c138a70be9ec9aa2ec471d929c23028cd143e67a
    Author: klillenb <kertlillenberk@gmail.com>
    Date:   Mon Nov 28 18:09:46 2022 +0200
    
        document filtering for employee view
    
    commit 5389c27a80ecf2f70cecd5e3c7eb4a086cec866a
    Author: klillenb <kertlillenberk@gmail.com>
    Date:   Sat Nov 26 17:57:03 2022 +0200
    
        querying based on role
    
    commit c33e9265d0f9627cd12a3ae6082d8d7b9284892e
    Author: klillenb <kertlillenberk@gmail.com>
    Date:   Sat Nov 26 16:42:44 2022 +0200
    
        more logic to adding shift
    
    commit 320231c97614bf5e121020cc57be4d2d2f7520e7
    Author: klillenb <kertlillenberk@gmail.com>
    Date:   Sat Nov 26 03:00:39 2022 +0200
    
        notes to do
    
    commit 0be70a0e5abc192327a38644882647ca6274ee31
    Author: klillenb <kertlillenberk@gmail.com>
    Date:   Sat Nov 26 02:18:24 2022 +0200
    
        fixed shift get
    
    commit 041ebe1ce76200229bea7370c04abce7715bd155
    Author: klillenb <kertlillenberk@gmail.com>
    Date:   Sat Nov 26 01:58:12 2022 +0200
    
        shift add accepts array

--- server/controllers/shift.controller.js
+++ server/controllers/shift.controller.js
@@ -6,6 +6,33 @@ exports.add = async (req, res) => {
     if(!errors.isEmpty()){
         return res.status(400).json({ errors: errors.array() })
     }
+    const shiftsArr = req.body.shifts
+    Object.keys(shiftsArr).forEach(shift => {
+        // hardcoded ://
+        switch (shiftsArr[shift].title) {
+            case "Day-Shift":
+                shiftsArr[shift].title = "dayShift"
+                break
+            case "Night-Shift":
+                shiftsArr[shift].title = "nightShift"
+                break
+            case "Booked":
+                shiftsArr[shift].title = "booked"
+                break
+            case "Leave":
+                shiftsArr[shift].title = "leave"
+                break
+            default:
+                return res.status(400).json(
+                    { errors: 
+                        `Was expecting 'Day-Shift', 'Night-Shift', 'Booked', 'Leave', but got ${shiftsArr[shift].title}!`
+                    })
+        }
+        shiftsArr[shift] = {
+            type: shiftsArr[shift].title,
+            date: shiftsArr[shift].date
+        }
+    })
     Shift.add(req.body, req.headers["authorization"].split(" ")[1])
         .then((data) => res.send(data))
         .catch((err) => res.send(err))
