bac4242b640112e4a7e2cf3cdce7c09a1e801945
Ragnar Valdre <roki1567@tlu.ee>
2022-12-14 17:01:24 +0200

    Login / Navbar test

--- front/src/tests/Login.test.js
+++ front/src/tests/Login.test.js
@@ -1,14 +1,21 @@
-import { render } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 import {BrowserRouter as Router} from 'react-router-dom'
 import Login from '../pages/Login'    
-import Dashboard from '../pages/Dashboard'
 
 
 
-test("Render Login page", async () => {
+
+test("Renders Login page, checks if login form has labels: Username, Password and if button has text value Login", async () => {
   render(
     <Router>
-      <Dashboard />
+      <Login />
     </Router>
   )
+
+  const Username = screen.getByText("Username")
+  const Password = screen.getByText("Password")
+  const LoginButton = screen.getByText("Login")
+  expect(Username).toBeInTheDocument()
+  expect(Password).toBeInTheDocument()
+  expect(LoginButton).toBeInTheDocument()
 })
